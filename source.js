a14817cf27583fd243061046d0e492704b3d16f9
pleft <panos.lefteratos@gmail.com>
2018-08-09 14:40:07 +0300

    initial commit

---
+++ source.js
@@ -0,0 +1,117 @@
+
+var game = new Phaser.Game(320, 240, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+    game.load.image('phaser', 'assets/shmup-ship.png');
+    game.load.image('bullet', 'assets/bullet0.png');
+
+}
+
+var sprite;
+var bullet;
+var bullets;
+var bulletTime = 0;
+
+//  Left, right and space key for controls
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
+var fireKey;
+var escKey;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#2d2d2d';
+
+    bullets = game.add.group();
+
+    bullets.enableBody = true;
+
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+    bullets.createMultiple(10, 'bullet');
+    bullets.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetBullet, this);
+    bullets.setAll('checkWorldBounds', true);
+
+    sprite = game.add.sprite(150, 180, 'phaser');
+
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+	
+	//  Register the keys.
+	this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+	this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+	this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+	this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+	this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.J);
+	this.escKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+
+    //  Stop the following keys from propagating up to the browser
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.J, Phaser.Keyboard.ESC ]);
+
+}
+
+function update() {
+
+    sprite.body.velocity.x = 0;
+    sprite.body.velocity.y = 0;
+
+    //  If true, it means that this key is down. If not, it means that the key is not down (was released/not pressed)
+    if (this.upKey.isDown)
+    {
+        sprite.body.velocity.y = -200;
+    }
+
+    if (this.downKey.isDown)
+    {
+        sprite.body.velocity.y = 200;
+    }
+
+    if (this.leftKey.isDown)
+    {
+        sprite.body.velocity.x = -200;
+    } 
+	
+    if (this.rightKey.isDown)
+    {
+        sprite.body.velocity.x = 200;
+    }
+
+    if (this.fireKey.isDown)
+    {
+        fireBullet();
+    }
+
+    if (this.escKey.isDown)
+    {
+        // nwjs related code to close the application
+        nw.App.closeAllWindows();
+    }
+}
+
+function fireBullet () {
+
+    if (game.time.now > bulletTime)
+    {
+        bullet = bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+            bullet.reset(sprite.x + 6, sprite.y - 8);
+            bullet.body.velocity.y = -300;
+            bulletTime = game.time.now + 250;
+        }
+    }
+
+}
+
+//  Called if the bullet goes out of the screen
+function resetBullet (bullet) {
+
+    bullet.kill();
+
+}
+
